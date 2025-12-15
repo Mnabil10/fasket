66b76dc0ee7c148281ff6ead3193cfe5a7a066dd
Mnabil10 <nabilm604@gmail.com>
2025-12-15 15:04:08 +0200

    add otp

--- src/main.ts
+++ src/main.ts
@@ -224,6 +224,8 @@ async function bootstrap() {
     'https://admin.fasket.cloud',
     'https://automation.fasket.cloud',
     'http://localhost:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'http://localhost:8100',
     'http://localhost:4200',
     'capacitor://localhost',
